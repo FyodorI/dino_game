9a5b8fe3fa86830ee6798e40ebd509d625df06cb
FyodorI <89913475+FyodorI@users.noreply.github.com>
2022-01-12 23:44:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+document.addEventListener ("keydown", function(event) {
+    jump();
+})
+function jump () {
+ if (dino.classList !="jump") {
+     dino.classList.add("jump")
+ }
+ setTimeout ( function () {
+     dino.classList.remove("jump")
+ },300)
+}
+
+let isAlive = setInterval (
+    function () {
+        let dinoTop = parseInt (window.getComputedStyle(dino).getPropertyValue("top"))
+        let cactusLeft = parseInt (window.getComputedStyle(cactus).getPropertyValue("left"))
+
+        if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+            alert ("GAME OVER")
+        }
+    }, 10)
+    
+    var counterVal = 0;
+
+    function incrementClick() {
+        updateDisplay(++counterVal);
+    }
+    
+    function resetCounter() {
+        counterVal = 0;
+        updateDisplay(counterVal);
+    }
+    
+    function updateDisplay(val) {
+        document.getElementById("counter-label").innerHTML = val;
+    }
